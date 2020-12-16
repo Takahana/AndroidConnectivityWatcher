e3a11672d573b26c9792d61465365a693a1145d7
Takahana <tagahana508@gmail.com>
2020-12-16 20:28:11 +0900

    add scope

--- app/src/main/java/tech/takahana/android/connectivitywatcher/MainViewModel.kt
+++ app/src/main/java/tech/takahana/android/connectivitywatcher/MainViewModel.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityScoped
 
+@ActivityScoped
 class MainViewModel @ViewModelInject constructor(
     @ApplicationContext appContext: Context
 ) : ViewModel() {
