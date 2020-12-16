62827ba7bff67a9cab802588e23119533d5cae9a
Takahana <tagahana508@gmail.com>
2020-12-16 20:20:52 +0900

    fix

--- app/src/main/java/tech/takahana/android/connectivitywatcher/MyApplication.kt
+++ app/src/main/java/tech/takahana/android/connectivitywatcher/MyApplication.kt
@@ -2,17 +2,6 @@ package tech.takahana.android.connectivitywatcher
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import tech.takahana.android.connectivitywatcher.di.AppComponent
-import tech.takahana.android.connectivitywatcher.di.DaggerAppComponent
 
 @HiltAndroidApp
-open class MyApplication : Application() {
-
-    val appComponent: AppComponent by lazy {
-        initializeComponent()
-    }
-
-    open fun initializeComponent(): AppComponent {
-        return DaggerAppComponent.factory().create(applicationContext)
-    }
-}
+open class MyApplication : Application()
